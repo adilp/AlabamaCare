4e37f90294b535d8b4c928f2f02e2f0abc6708d6
Adil Patel <>
2021-06-08 18:22:24 -0400

    added copy url

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,20 +1,33 @@
 import "./css/ItemDetail.css";
 import React, { useState, useEffect } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import Header from "./Header";
 import ReactPlayer from "react-player";
 import Posts from "./Posts";
 import sanityClient from "../utils/client";
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 import utils from "../utils/utils";
 import HashtagComponent from "./common/HashtagComponent";
 import SubmittedByComponent from "./common/SubmittedByComponent";
+import URL from "../utils/constants";
 
 const ItemDetail = (props = null) => {
   let { title } = useParams();
+  const location = useLocation();
+  const path = `${URL}${location.pathname}`;
+  console.log(location.pathname);
   let querry = `_type == "comment" && _id == "${title}"`;
   let data;
   const [videoData, setvideo] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [isCopied, setIsCopied] = useState(false);
+
+  const onCopyText = () => {
+    setIsCopied(true);
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1000);
+  };
 
   useEffect(() => {
     sanityClient
@@ -55,6 +68,13 @@ const ItemDetail = (props = null) => {
             />
           </div>
           <span className="postinfo__upvote">{upvote}</span>
+
+          <CopyToClipboard text={path} onCopy={onCopyText}>
+            <div className="copy-area">
+              <button>Copy to Clipboard</button>
+              {isCopied ? "Copied" : ""}
+            </div>
+          </CopyToClipboard>
         </div>
         <div className="itemDetail__related">
           <span>Related: </span>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
 const constants = {
+  URL: "http://localhost:3000",
   REGEX: {
     TIME: /(?<!\S)(?:(?:(\d{1,2}):)?([0-5]?\d):)?([0-5]?\d)(?!\S)/gm,
     HASHTAG: /(?:^|\s)(?:#)([a-zA-Z\d]+)/gm,
