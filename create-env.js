79016f17bf4943e8282f4c802a5f9cdeaa6bda38
Adil Patel <>
2021-06-18 17:42:29 -0500

    fixed env typo

--- create-env.js
+++ create-env.js
@@ -4,6 +4,6 @@ const vars = `
  REACT_APP_PROJECT_ID=${process.env.ENV_VAR_1_NETLIFY}\n
  REACT_APP_DATASET=${process.env.ENV_VAR_2_NETLIFY}\n
  REACT_APP_TOKEN=${process.env.ENV_VAR_3_NETLIFY}\n
- REACT_APP_GA_TRACKING_CODE=${process.env.ENV_VAR4_NETLIFY}
+ REACT_APP_GA_TRACKING_CODE=${process.env.ENV_VAR_4_NETLIFY}
 `;
 fs.writeFileSync(path, vars);
