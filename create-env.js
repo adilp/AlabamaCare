d77af1838ca89691fbd0a391cfea8da120a72ab9
Adil Patel <>
2021-06-18 17:20:50 -0500

    added env

--- create-env.js
+++ create-env.js
@@ -3,7 +3,7 @@ const path = `./.env`;
 const vars = `
  REACT_APP_PROJECT_ID=${process.env.ENV_VAR_1_NETLIFY}\n
  REACT_APP_DATASET=${process.env.ENV_VAR_2_NETLIFY}\n
- REACT_APP_TOKEN=${process.ENV_VAR_3_NETLIFY}\n
- REACT_APP_GA_TRACKING_CODE=${process.ENV_VAR4_NETLIFY}
+ REACT_APP_TOKEN=${process.env.ENV_VAR_3_NETLIFY}\n
+ REACT_APP_GA_TRACKING_CODE=${process.env.ENV_VAR4_NETLIFY}
 `;
 fs.writeFileSync(path, vars);
